f34f3a9654055f9146a69369f94e093d3452fbbe
Brad Wilson <dotnetguy@gmail.com>
2015-06-28 11:00:33 -0700

    Switch to a CMD file to run all test, so it ignores (expected) failures

---
+++ RunAllTests.cmd
@@ -0,0 +1,4 @@
+@echo off
+packages\xunit.runner.console\tools\xunit.console ConsoleRunner\bin\Debug\ConsoleRunner.dll
+msbuild Tests.proj /nologo /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+dnx DNXRunner test
